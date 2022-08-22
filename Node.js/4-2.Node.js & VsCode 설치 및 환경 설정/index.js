bb23af6385ac0dee8e568911b48044bc185707f3
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-08-22 22:27:00 +0900

    #section4: 4-2 Node.js & VsCode 설치 및 환경 설정

---
+++ Node.js/4-2.Node.js & VsCode 설치 및 환경 설정/index.js
@@ -0,0 +1,16 @@
+// Node.js 버전
+// LTS 버전 : 안전성과 신뢰성을 보장하는 버전 / 실제 서비스용
+// 현재(Current) 버전 : 새롭게 추가된 기능들을에대해 최신 버전을 사용할 수 있음(LTS 버전에서 사용하던 기능이 사라지거나 변경될 수 있음)
+
+console.log("Hello VSCode");
+
+function a() {}
+
+console.log("calc");
+
+// 확장 프로그램
+// Prettier - Code formatter
+// Editor: Format On Save (자동 저장)
+
+// Material Icon Theme
+// VSCode 아이콘 변경
