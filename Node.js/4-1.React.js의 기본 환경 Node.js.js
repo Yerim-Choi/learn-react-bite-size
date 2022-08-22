57e698b460def59b247189cf77f47302334a3160
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-08-22 22:06:48 +0900

    #section4: 4-1 React.js의 기본 환경 Node.js

---
+++ Node.js/4-1.React.js의 기본 환경 Node.js.js
@@ -0,0 +1,19 @@
+// 자바스크립트 코드는 브라우저 내장 자바스크립트 엔진을 이용하여 실행
+// Safari Apple / Nitro
+// FireFox Mozilla // Spider Monkey
+// Chrome Google // V8 ** (대표)
+// Edge Microsoft // Chakra
+// Opera Software // Presto
+
+// Node.js
+// 브라우저가 아닌 환경에서 자바스크립트 실행 (V8 엔진)
+// 자바스크립트의 실행환경 JavaScript's Runtime
+// 자바스크립트로 Web Server도 만들 수 있음
+// Browser -- 요청(Request) / 응답 (Response) -- Web Server
+
+// 브라우저 -- 웹 -- 웹서버
+// 핸드폰 -- 미디어 미디어 -- 서버
+// 핸드폰 -- 채팅 데이터 -- 채팅 서버
+
+// React
+// 자바스크립트 파일들은 복잡한 구조를 웹 상에서 프로그램 처럼 돌아감 -> 웹 어플리케이션 or 리액트 어플리케이션
