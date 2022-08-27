1a0856b9c33a28146d2e5dc7e63ab154c1b2bb0d
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-08-27 23:13:49 +0900

    #section5: 5-2 Create React App

---
+++ React/React 입문/5-2.Create React App.js
@@ -0,0 +1,23 @@
+// Create React App
+
+// Boiler Plate
+// 이미 세팅 완료된 패키지
+// 마치 보일러를 찍어내듯, 서비스를 개발할 수 있는 빵 틀의 역할을 하는 패키지를 의미 함
+
+// React.js
+// Node 기반의 Javascript UI 라이브러리
+
+// Webpack
+// 다수의 자바스크립트 파일을 하나의 파일로 합쳐주는 모듈 번들 라이브러리
+
+// Babel
+// JSX등의 쉽고 직관적인 자바스크립트 문법을 사용할 수 있도록 해주는 라이브러리
+
+// npm start
+// node.js 기반의 웹 서버에서 동작 (http://localhost:3000/)
+
+// - node_modules 없을 시 npm i (npm install) 를 통해 다운로드
+// - public
+// - src
+
+// import export 컴포넌트 방식으로 작업 / ES Module
