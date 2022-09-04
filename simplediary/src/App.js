56bc74c4ffd95fa1aba21d1261eee74c2fd0d5c2
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-09-04 20:47:56 +0900

    #section6: 6-4 React에서 배열 사용하기 2 - 데이터 추가하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,43 +1,61 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "야옹이",
-    content: "오늘 츄르를 먹었다",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "댕댕이",
-    content: "오늘 산책을 했다",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "야옹이2",
-    content: "오늘 캣타워에 올라갔다",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "야옹이3",
-    content: "냥냥펀치",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-];
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "야옹이",
+//     content: "오늘 츄르를 먹었다",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "댕댕이",
+//     content: "오늘 산책을 했다",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "야옹이2",
+//     content: "오늘 캣타워에 올라갔다",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "야옹이3",
+//     content: "냥냥펀치",
+//     emotion: 3,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
   return (
     <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList diaryList={data} />
     </div>
   );
 }
