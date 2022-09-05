36ee83e03f480d850f0fddb8d81443cb8317616e
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2022-09-05 21:51:30 +0900

    #section6: 6-5 React에서 배열 사용하기 3 - 데이터 삭제하기

--- simplediary/src/DiaryItem.js
+++ simplediary/src/DiaryItem.js
@@ -1,14 +1,30 @@
-const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+const DiaryItem = ({
+  onDelete,
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
         <span>
-          작성자 : {author} | 감정 정수 : {emotion}{" "}
+          작성자 : {author} | 감정 정수 : {emotion}
         </span>
         <br />
       </div>
       <span className="date">{new Date(created_date).toLocaleString()}</span>
-      <div className="content">감정 : {content}</div>
+      <div className="content">{content}</div>
+      <button
+        onClick={() => {
+          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
